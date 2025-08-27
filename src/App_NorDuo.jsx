7bb6620f492ee26367295d98282da8d515c811c9
nonjc0000 <nonjc0000@gmail.com>
2025-08-27 11:53:08 +0800

    create nav, footer components

---
+++ src/App_NorDuo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+const App_NorDuo = () => {
+    return (
+        <div className='wrap'>
+            <NavBar />
+            <div>App_NorDuo</div>
+            <Footer />
+        </div>
+    )
+}
+
+export default App_NorDuo
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='nav_wrap'>
+        <img src="logo.png" alt="Logo" />
+
+    </div>
+  )
+}
+
+export default NavBar
