afbda92a4a2974c01f2513d273b8cb4bfca39174
nonjc0000 <nonjc0000@gmail.com>
2025-09-10 09:25:44 +0800

    0910

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
     <section className='hero_section_wrap'>
       <div className='hero_content'>
         <video src="./videos/band_performance.mp4" autoPlay loop muted playsInline className='hero_vid' />
-        
+
         <div className="deco_top">
           <figure className="deco_arrow">
             <img src="./images/HeroSection/deco_arrow.svg" alt="" />
@@ -14,18 +14,18 @@ const HeroSection = () => {
             <figure className='rec_dot'>
               <img src="./images/HeroSection/rec_dot.svg" alt="" />
             </figure>
-  
+
             <span className='rec_text'>REC</span>
           </div>
         </div>
-        
+
         <div className="deco_bottom">
           <div className="deco_slogan">
             <p className='slogan_line'>We play.</p>
             <p className='slogan_line'>We create.</p>
             <div className='slogan_decoration'>
               <figure className='decoration_lines'>
-                <img src="./images/HeroSection/decoration_lines.svg" alt="" />
+                <img src="./images/deco/decoration_lines.svg" alt="" />
               </figure>
             </div>
           </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SoundCreator from './SoundCreator';
 import HeroSection from './HeroSection';
+import About_us from './About_us';
 
 const Home = () => {
   return (
     <>
     <SoundCreator/>
     <HeroSection/>
+    <About_us/>
     </>
   )
 }
