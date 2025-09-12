c05e7c2fa00ed86891638ca26cafee35612ec760
nonjc0000 <nonjc0000@gmail.com>
2025-09-12 14:53:37 +0800

    0912

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -4,16 +4,17 @@ const Learning = () => {
     return (
         <div className="learning_wrap">{/* 背景三角形 */}
             <svg className="bg_triangle" viewBox="0 0 100 100" preserveAspectRatio="none">
-                <polygon points="100,0 100,0 100,100 0,100" fill='white'/>
+                <polygon points="100,0 100,0 100,100 0,100" fill='none' />
             </svg>
 
             <div className="learning_content_box">
                 <div className="text_box">
                     <h1><span>Learn</span> with us!</h1>
                     <div className="desc_box">
-                        <p>Find Your Sound. Play Your Style.</p>
+                        <p>Find Your Sound. <br />
+                            Play Your Style.</p>
                         <p>We help you master riffs, rhythm, and improvisation—anytime, anywhere.</p>
-                        <p>Go check our online courses ⟶</p>
+                        <p>Go check our online <br />courses ⟶</p>
                     </div>
                 </div>
                 <div className="card_box">
