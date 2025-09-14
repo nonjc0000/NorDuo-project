91a15e4dd8faf11238172c465e90d33c76292050
nonjc0000 <nonjc0000@gmail.com>
2025-09-14 19:35:46 +0800

    0914

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -9,10 +9,12 @@ const Shop = () => {
             <div className='desc_box'>
                 <h1>New Arrival</h1>
                 <p>Find Your Sound.<br />Play Your Style.</p>
+                <div className='product_container'>
                 <figure className='product_box'>
                     <img src="/images/shop/guitar.png" alt="" />
-                    <figcaption>Foundwave Player II Terecaster </figcaption>
                 </figure>
+                <p>Foundwave Player II Terecaster </p>
+                </div>
             </div>
 
             <div className='categories_box'>
@@ -20,19 +22,19 @@ const Shop = () => {
 
                 <div className="categories_list">
                     <div className="catagories_item">
-                        <h3>Guitar</h3>
+                        <h3 className='guitar'>Guitar</h3>
                         <h3>[01]</h3>
                     </div>
                     <div className="catagories_item">
-                        <h3>Bass</h3>
+                        <h3 className='bass'>Bass</h3>
                         <h3>[02]</h3>
                     </div>
                     <div className="catagories_item">
-                        <h3>Keyboard</h3>
+                        <h3 className='kb'>Keyboard</h3>
                         <h3>[03]</h3>
                     </div>
                     <div className="catagories_item">
-                        <h3>Others</h3>
+                        <h3 className='others'>Others</h3>
                         <h3>[04]</h3>
                     </div>
                 </div>
